e137f577ce97b2b7905e6f28c4f432d79ed959f6
Leonardo <leomeireles66@gmail.com>
2025-04-18 11:06:44 -0300

    Validating Reading Step

---
+++ user/src/main/java/com/leonardo/user_request_spring_batch/writer/InsertUserDataDBWriterConfig.java
@@ -0,0 +1,17 @@
+package com.leonardo.user_request_spring_batch.writer;
+
+import com.leonardo.user_request_spring_batch.dto.UserDTO;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class InsertUserDataDBWriterConfig{
+
+
+    @Bean
+    public ItemWriter<UserDTO> insertUserDataDBWriter(){
+        return users -> users.forEach(System.out::println);
+    }
+
+}
